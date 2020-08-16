7856bcd6802280624fe306ed90a7ce1da7eebcdf
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-16 20:32:56 +0800

    <feat>[add Rspec] init

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index" 
-  # 開發期間需要常常測試預覽寄出的Email內容，但是實際寄送出去又很沒效率。使用letter_opener
+  
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
