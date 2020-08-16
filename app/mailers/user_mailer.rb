147c74ca7ca1b4c5a0804fe60958891e402ecd60
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-16 17:13:22 +0800

    <feat>[add devise confirmable] send account confirm email to user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,7 +9,7 @@ class UserMailer < ActionMailer::Base
   def notify_comment(user, comment)
     @greeting = "Hi"
     @comment = comment
-    mail(:to => user.email, :subject => "New Comment")
+    mail(:to => user.email, :subject => "恭喜您註冊成功！PMaster 最有趣的專案管理網站")
     # mail to: "to@example.org"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index" 
+  # 開發期間需要常常測試預覽寄出的Email內容，但是實際寄送出去又很沒效率。使用letter_opener
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
-  root to: "home#index" 
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
 end
