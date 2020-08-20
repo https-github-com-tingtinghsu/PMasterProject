9343df38bbec517260e6719cfe64ef935afcd47c
ice12031110 <ice12031110@gmail.com>
2020-08-20 20:42:50 +0800

    <feat>初步新增Vue路徑 exit qq :q :wq :q exit

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -17,3 +17,21 @@ const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
 
 // import "scripts/shared";
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  const app = new Vue({
+    el: '#hello',
+    data: () => {
+      return {
+        message: "Can you say hello?"
+      }
+    },
+    components: { App },
+    template:'<App />'
+  })
+})
