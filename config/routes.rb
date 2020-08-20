1f89f5f16622c45c679ea820ae395d46c6b3e8b6
ice12031110 <ice12031110@gmail.com>
2020-08-20 20:45:24 +0800

    <feat>初步新增Vue路徑-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index" 
+  # get"/test" ,to:"pages#index"
   resources :pages, only:[:index]
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
