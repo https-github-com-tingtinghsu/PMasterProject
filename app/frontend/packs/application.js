4557246b5480c193a061b81382ec84fe951358de
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-27 23:37:58 +0800

    <fix>[import application.js] 修正漢堡選單在小螢幕顯示的js效果

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -11,6 +11,7 @@ require("channels")
 require("scripts")
 require("stylesheets")
 
+import '../scripts/shared/application'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
