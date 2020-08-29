ecf6bb096980239284bc218791296938846c3190
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-29 08:27:13 +0800

    <feat>[add tailwind.css] also remove empty migration

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -12,6 +12,8 @@ require("scripts")
 require("stylesheets")
 
 import '../scripts/shared/application'
+import 'scripts'
+import 'stylesheets'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- db/migrate/20200828074043_add_omniauth_to_users.rb
+++
@@ -1,4 +0,0 @@
-class AddOmniauthToUsers < ActiveRecord::Migration[6.0]
-  def change  
-  end
-end
