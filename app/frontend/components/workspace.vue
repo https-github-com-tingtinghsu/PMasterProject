398cb2b595fbce920e6e47702a7e8b702c592219
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-08-30 21:50:56 +0800

    <feat>[create workspace] conponent

---
+++ app/frontend/components/workspace.vue
@@ -0,0 +1,15 @@
+<template>
+<h2>{{ workspace.name }} </h2>
+</template>
+
+
+<script>
+export default {
+  name: 'Workspace',
+  props: ["workspace"]
+};
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
