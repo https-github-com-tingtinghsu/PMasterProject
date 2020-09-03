496d94aaf6e80a8425aaea2c2e98423796a8258d
ice12031110 <ice12031110@gmail.com>
2020-09-03 13:49:00 +0800

    <feat>初步完成非同步聊天

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,16 +25,9 @@ class MessagesController < ApplicationController
   # POST /messages.json
   def create
     @message = Message.new(message_params)
-
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to @message, notice: 'Message was successfully created.' }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
+    @message.user = current_user
+    @message.save
+    redirect_to request.referrer
   end
 
   # PATCH/PUT /messages/1
