7cd4d6a1bcbb347e56d850ca8113dacc1d61b1e9
ice12031110 <ice12031110@gmail.com>
2020-09-03 15:21:42 +0800

    <feat>整理chatroom的外觀

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -14,6 +14,7 @@ require("stylesheets")
 import '../scripts/shared/application'
 import 'scripts'
 import 'stylesheets'
+import '../stylesheets/index'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
