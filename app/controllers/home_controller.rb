710b6691b78788165fb0a950fb6960d69cc6815e
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-09-04 12:28:25 +0800

    <feat>[workspace] 2column

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    redirect_to workspaces_path if current_user
+    # redirect_to workspaces_path if current_user
+    # byebug
+    # redirect_to demo_path if current_user
   end
 end
---
+++ app/frontend/scripts/sidebar.js
@@ -0,0 +1,18 @@
+document.addEventListener('turbolinks:load', () => {
+  $.ajax({url: "/workspaces", success: function(result){
+    result.forEach(element => 
+      $("#sidebar").append(generateWorkspace(element))
+    )
+  }});
+})
+
+function generateWorkspace(workspace){
+  return `
+    <a class="panel-block is-active" data-workspace-id="${workspace.id} ">
+      <span class="panel-icon">
+      <i class="fas fa-book" aria-hidden="true"></i>
+      </span>
+      ${workspace.name} 
+    </a>
+  `
+}
