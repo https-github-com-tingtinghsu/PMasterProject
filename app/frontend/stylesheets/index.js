d5a4367590fcbfd5f194814a2501fdaa24bde498
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-09-04 22:47:46 +0800

    <fix>[tailwind] webpacker

--- app/frontend/stylesheets/index.js
+++ app/frontend/stylesheets/index.js
@@ -3,3 +3,9 @@ import "@fortawesome/fontawesome-free/css/all.css"
 import "tailwindcss/base";
 import "tailwindcss/components";
 import "tailwindcss/utilities";
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+}
