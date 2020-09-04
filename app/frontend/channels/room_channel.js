f82bc56f3ba72cd1f76d154601e0dfde2f322c26
ice12031110 <ice12031110@gmail.com>
2020-09-04 09:41:41 +0800

    redis安裝，確認websocket前置

---
+++ app/frontend/channels/room_channel.js
@@ -0,0 +1,16 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create( { channel: "RoomChannel", room_id: 2}, {
+  connected() {
+    console.log("aaa")
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    console.log(data)
+  }
+});
