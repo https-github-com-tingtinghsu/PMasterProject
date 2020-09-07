f8c15fcaf295df3d4a9c35ab81bce1506ac0b39e
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-09-07 11:10:24 +0800

    <fix> [dashboard] application index and layout

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
   def index
-    
+    @board = Board.find(params[:board_id])
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
+  
   def index
     redirect_to dashboard_path if current_user
+  end
+
+  def dashboard
+    
   end
 end
