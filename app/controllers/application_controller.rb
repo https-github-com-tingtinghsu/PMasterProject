cfbe6228fff28049142548b918d7a3e8f66e80c9
Mei <mei@Hui-TingdeMacBook-Air.local>
2020-09-07 21:11:46 +0800

    <feat>groupCRUD Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  #rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   private
   def not_found
-    redirect_to workspaces_path, notice: '抱歉，資料不存在'
+    #redirect_to workspaces_path, notice: '抱歉，資料不存在'
   end
 end
 
