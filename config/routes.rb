55111ccc6a492ec05a105733fa9c3f5c54dbf7b5
Mei Yeh <yeha90822@gmail.com>
2020-09-08 12:05:20 +0800

    <feat>itemCRUD_Done

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   resources :workspaces, except: [:new, :edit, :show] do
     get "add_member"
     resources :boards, shallow: true do
-      resources :groups, shallow: true
+      resources :groups, shallow: true do
+        resources :items, shallow: true
+      end
     end
   end
 
