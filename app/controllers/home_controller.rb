55c8713d871895ba508478b5cd200a3d35983828
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-09-09 23:31:54 +0800

    <feat> [email] invite user to workspace who is not member yet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,7 @@ class HomeController < ApplicationController
   end
 
   def dashboard
+    # byebug 
     # 有cookies的人才需要confirmed_invitation
     confirmed_invitation if cookies[:user_token]
 
@@ -17,12 +18,14 @@ class HomeController < ApplicationController
   private
   def confirmed_invitation
     confirmed_invitation = Invitation.find_by(token: cookies[:user_token])
-    if current_user 
+    if current_user
+
       # 排除了「如果已經透過同一個token被加入工作區，就不要再重複加同一個user進去」的狀況
       if confirmed_invitation.workspace.users.where(id: current_user.id).size == 0
         confirmed_invitation.receive_user_id = current_user.id
         confirmed_invitation.workspace.users << current_user
-        flash[:add_board_notice] = "成功加入#{@confirmed_invitation.workspace.name}看板！"
+        flash[:add_board_notice] = "成功加入#{confirmed_invitation.workspace.name}看板！"
+        #confirmed_invitation.workspace_id
       end
     end    
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def token_storage
-    cookies[:user_token] = params[:t]
+    # token存在的話才存入cookies, 不然有的時候token為null就找不到看板
+    cookies[:user_token] = params[:t] if params[:t].present?
   end
 end
