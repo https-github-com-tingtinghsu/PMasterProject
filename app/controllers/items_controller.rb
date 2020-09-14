a8a536f882a331971399969ea285c6b09eb4e9fe
Mei Yeh <yeha90822@gmail.com>
2020-09-14 13:27:28 +0800

    AssignmentCheckbox有ID

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-	before_action :find_board_and_group, only: [:index, :create]
+	before_action :find_board_and_group, only: [:index, :new, :create]
 	before_action :find_item, only: [:edit, :update, :destroy]
 
 	def index
@@ -8,10 +8,12 @@ class ItemsController < ApplicationController
 	end
 	def new
 		@item = Item.new
+		@workspace_users = Workspace.find_by(id: @board.workspace_id).users
 	end
 	def create
 		@item = @group.items.new(item_params)
 		if @item.save
+			byebug
 			redirect_to board_groups_path(@group.board_id), notice: "新增成功"
 			# 新增function連動github issuse
 		else
@@ -19,14 +21,19 @@ class ItemsController < ApplicationController
 		end
 	end
 	def edit
+		@group = Group.find(@item.group_id)
+		@board = Board.find(@group.board_id)
+		@workspace_users = Workspace.find_by(id: @board.workspace_id).users
 	end
 	def update
 		if @item.update(item_params)
 			board =	Board.find(Group.find(@item.group_id).board_id)
+			# 先找到該item隸屬的group，再找該group隸屬的board，以便儲存後轉址到 boards/id/groups
 			redirect_to board_groups_path(board)
 		else
 			render :edit
 		end
+		# byebug
 	end
 	def destroy
 		@item.destroy
@@ -42,7 +49,6 @@ class ItemsController < ApplicationController
 		@board = @group.board
 	end
 	def item_params
-		# byebug
 		params.require(:item).permit(:name, :description, :status, :person, :due_date)
 	end
 end
