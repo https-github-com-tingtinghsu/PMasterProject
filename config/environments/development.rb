5d8051d8059b5680af0ce117e8b058a77324136e
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-09-17 14:58:32 +0800

    <feat> move mailgun api to production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,25 +38,17 @@ Rails.application.configure do
   # https://ihower.tw/rails/actionmailer.html  
   # config.action_mailer.delivery_method = :smtp
 
-  # config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.default_url_options = { host: 'localhost', port: 3333 }
   # config.action_mailer.default_url_options = { host: 'mail.pmaster.tw' } 
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-    api_key: ENV["mailgun_myapikey"],
-    domain: 'mail.pmaster.tw'
-    # api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
-  }
-
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.mailgun.org',
-  #   port:                 587,
-  #   domain:               '5xruby.tw',
-  #   user_name:            ENV["mailgun_user_name"],
-  #   password:             ENV["mailgun_password"],
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true
+  # config.action_mailer.delivery_method = :mailgun
+  # config.action_mailer.mailgun_settings = {
+  #   api_key: ENV["mailgun_myapikey"],
+  #   domain: 'mail.pmaster.tw'
+  #   # api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
   # }
+
+
 #  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
