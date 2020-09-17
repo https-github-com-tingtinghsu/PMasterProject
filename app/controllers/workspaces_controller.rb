33cd6325f6260ca13f43f419b8f552ed7d24798c
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-09-17 14:30:42 +0800

    <feat> delay job letter_opener

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -68,15 +68,15 @@ class WorkspacesController < ApplicationController
       # @workspace.users << find_user
 
       message = "success"
-      UserMailer.invite_member(@invitation).deliver_now!
+      # UserMailer.invite_member(@invitation).deliver_now!
       # UserMailer.notify_comment(User.find(9), "hi console").deliver_now!
-      # InviteMemberMailJob.set(wait: 2.seconds).perform_later(@invitation)
+      InviteMemberMailJob.set(wait: 2.seconds).perform_later(@invitation)
 
     else
       result = true 
       message = "success"
       UserMailer.invite_new(@invitation).deliver_now!
-      # InviteNewMailJob.set(wait: 2.seconds).perform_later(@invitation)
+      InviteNewMailJob.set(wait: 2.seconds).perform_later(@invitation)
     end
     render json: { 
       success: result,
