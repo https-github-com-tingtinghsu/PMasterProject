6e94f956b4bfd6ee9bcb84ed574f8be265edfc0c
Mei Yeh <yeha90822@gmail.com>
2020-09-17 14:33:53 +0800

    <fix>posts的關閉鈕修復

--- app/frontend/scripts/shared/post.js
+++ app/frontend/scripts/shared/post.js
@@ -1,17 +1,10 @@
 document.addEventListener('turbolinks:load', () => {
 	let postIcon = document.querySelectorAll('.postIcon')
-	let PostBtnCancel = document.querySelector('#abc123')
 	if (postIcon){
 		postIcon.forEach(element => {
-      element.addEventListener('click', function(){
-        document.querySelector('#posts-block').classList.add('is-active-for-posts-block')
+			element.addEventListener('click', function(){
+				document.querySelector('#posts-block').classList.add('is-active-for-posts-block')
       })
-		})
-	}
-	if (PostBtnCancel){	
-		PostBtnCancel.addEventListener('click',function(){
-			console.log(PostBtnCancel)
-			document.querySelector('#posts-block').classList.remove('is-active-for-posts-block')
 		})
 	}
 });
