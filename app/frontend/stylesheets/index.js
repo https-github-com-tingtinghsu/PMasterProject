257a3b848091c2a04830318a0672656201a5e248
ice12031110 <ice12031110@gmail.com>
2020-09-23 17:58:05 +0800

    <fix>chatroom_creator_isonline<feat>form樣式

--- app/frontend/stylesheets/index.js
+++ app/frontend/stylesheets/index.js
@@ -4,5 +4,6 @@ import "tailwindcss/base";
 import "tailwindcss/components";
 import "tailwindcss/utilities";
 import "./chat_room.scss";
-import "./item_table.css"
-import "./posts_block.css"
+import "./item_table.css";
+import "./posts_block.css";
+import "./all_pages.scss";
