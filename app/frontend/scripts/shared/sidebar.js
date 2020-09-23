f1a1caab6177b499359034d60b5609808429a795
ice12031110 <ice12031110@gmail.com>
2020-09-23 18:16:21 +0800

    <fix>解決console小問題

--- app/frontend/scripts/shared/sidebar.js
+++ app/frontend/scripts/shared/sidebar.js
@@ -129,7 +129,7 @@ function createSidebarRow(model, isCreated = true, isWorkspace = true){
       <i class="far fa-folder" id="folder-${model.id}"></i>
     </a>
   `).click(function(){
-    console.log($(this).parent())
+    // console.log($(this).parent())
     // 按一下時，給相反的值 (false -> true; true -> false)
     $(this).data("toggle", !$(this).data("toggle")) 
     if($(this).data("toggle")) {
