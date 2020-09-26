7bb83d7019bb778623b9dabd566f4d1c8195e9ec
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-09-26 19:16:07 +0800

    <feat>[chart] show button when items have points

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,10 @@ class Group < ApplicationRecord
   has_many :items, dependent: :destroy
 
   validates :name, presence: true
+
+  def has_chart?
+    return false if items.size == 0
+    # 有item, 而且每個item要有point, 才能畫圖
+    items.all? { |item| item.point.present? }
+  end
 end
