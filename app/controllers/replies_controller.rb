cfb9781cede60eb65f881e2179512dcdab19fea1
mei <yeha90822@gmail.com>
2020-09-27 12:06:56 +0800

    <feat>replies_can_be_created

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,17 +1,21 @@
 class RepliesController < ApplicationController
+  before_action :find_post, only: [:create, :destroy]
   before_action :find_reply, only: [:update]
-  
   def create
     @reply = @post.replies.new(content: params[:replycontent])
 		@reply.user_id = current_user.id
 		@reply.save
   end
-
+  
   def destroy
     @reply.destroy
   end
-
+  
   private
+  
+    def find_post
+      @post = Post.find(params[:post_id])
+    end
 
     def find_reply
       # @reply = Reply.find_by(params[:id])...
