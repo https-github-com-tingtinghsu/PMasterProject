19fddb46aab91ded5b5304fe414c1df97c99bbb1
Kevin <k3vinwei@gmail.com>
2020-10-01 01:22:20 +0800

    <feat>[Notification_Like-Wei]完close console.log

--- app/frontend/channels/post_channel.js
+++ app/frontend/channels/post_channel.js
@@ -2,19 +2,12 @@ import consumer from "../../javascript/channels/consumer"
 
 consumer.subscriptions.create( { channel: "PostChannel" }, {
   connected() {
-    console.log(" post connect")
-    // Called when the subscription is ready for use on the server
   },
 
   disconnected() {
-    // Called when the subscription has been terminated by the server
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    console.log(data)
-
-    // post-id-18-be-liked-counts
     const element = document.querySelector('#post-id-' + data.postid + '-be-liked-counts')
     console.log(element)
     if(!element)return
