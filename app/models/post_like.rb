741b501a6369f75612031abc4d853cac549e36a2
Kevin <k3vinwei@gmail.com>
2020-09-30 21:10:09 +0800

    新增like 數量function

--- app/models/post_like.rb
+++ app/models/post_like.rb
@@ -2,4 +2,6 @@ class PostLike < ApplicationRecord
   belongs_to :post
   belongs_to :user
   validates_uniqueness_of :user_id, :scope => :post_id
+
+  scope :count_like, ->(p) { where(["post_id = ?", p]).count() }
 end
