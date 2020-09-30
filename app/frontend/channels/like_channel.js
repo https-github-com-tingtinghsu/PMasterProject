cd7cacd222f6f7ca5b99f084b46e4e725c235f75
Kevin <k3vinwei@gmail.com>
2020-10-01 01:18:27 +0800

    <feat>[Notification_Like-Wei]完reset like channel

--- app/frontend/channels/like_channel.js
+++ app/frontend/channels/like_channel.js
@@ -2,20 +2,11 @@ import consumer from "../../javascript/channels/consumer"
 
 consumer.subscriptions.create("LikeChannel", {
   connected() {
-    // Called when the subscription is ready for use on the server
   },
 
   disconnected() {
-    // Called when the subscription has been terminated by the server
   },
 
   received(data) {
-    console.log(data)
-
-    // post-id-18-be-liked-counts
-    const element = document.querySelector('#post-id-18-be-liked-counts')
-    console.log(element)
-    if(!element)return
-    element.textContent = "100";
   }
 });
