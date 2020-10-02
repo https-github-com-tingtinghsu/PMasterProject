5a759c177a2c16e8ca00cb2d01ef7a3d364512ab
Kevin <k3vinwei@gmail.com>
2020-10-02 13:18:29 +0800

    f<fix>[reply_display_name-Wei]修正

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,6 +1,7 @@
 class RepliesController < ApplicationController
   before_action :find_post, only: [:create, :destroy]
   before_action :find_reply, only: [:update]
+  
   def create
     @reply = @post.replies.new(content: params[:replycontent])
 		@reply.user_id = current_user.id
@@ -12,13 +13,12 @@ class RepliesController < ApplicationController
   end
   
   private
-  
-    def find_post
-      @post = Post.find(params[:post_id])
-    end
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
 
-    def find_reply
-      # @reply = Reply.find_by(params[:id])...
-    end
+  def find_reply
+    # @reply = Reply.find_by(params[:id])...
+  end
 
 end
