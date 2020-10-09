613526efa6a981f6c558d1fb372ddcc9f19f4856
Kevin <k3vinwei@gmail.com>
2020-10-09 17:02:43 +0800

    test onclick but reload ....

--- app/frontend/channels/chart_channel.js
+++ app/frontend/channels/chart_channel.js
@@ -10,14 +10,27 @@ consumer.subscriptions.create({ channel: "ChartChannel"}, {
   },
 
   received(data) {
-    console.log(data)
     const element = document.querySelector('.subtitle')
     if(!element)return
     const group_id = element.getAttribute('data-group-id')
     if(!group_id)return
     if( group_id == data.groupid){
       window.location.reload();
+
+      // let burn_element = document.getElementsByClassName('burn-chart-tab is-active')
+      // let pie_element = document.getElementsByClassName('pie-chart-tab is-active')
+      // let bar_element = document.getElementsByClassName('bar-chart-tab is-active')
+
+      // if(burn_element.length > 0){
+      //   $('.burn-chart-tab').on('click',() => {
+
+      //   })
+      // }
+      // if(pie_element.length > 0){
+      //   $('.pie-chart-tab').on('click',() => {
+          
+      //   })
+      // }
     }
-    console.log('group_id : ' + group_id + ' data.group_id : ' + data.groupid)
   }
 });
