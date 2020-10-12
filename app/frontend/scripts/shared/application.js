2dafe618040290ee4fdc472dc40e115fd6e8dfe0
ice12031110 <ice12031110@gmail.com>
2020-10-12 10:39:06 +0800

    <fix>item_arrow_rotate_270egd

--- app/frontend/scripts/shared/application.js
+++ app/frontend/scripts/shared/application.js
@@ -190,7 +190,7 @@ document.addEventListener('turbolinks:load', () => {
   const workspaceToggle = document.querySelector('.workspace-list-colse')
   const workspaceList = document.querySelector('.media-display-none')
   workspaceToggle.addEventListener('click', () => {
-    workspaceToggle.classList.toggle('arrow')
+    workspaceToggle.classList.toggle('workspce_list_arrow')
     if (document.querySelector('#media-display-none') == null ){
       workspaceList.setAttribute('id','media-display-none')
     }else{
