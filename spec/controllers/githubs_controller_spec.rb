a6e95f51bf22537007160728264c07076b68570d
Kevin <k3vinwei@gmail.com>
2020-10-14 00:55:23 +0800

    add temp file

--- spec/controllers/githubs_controller_spec.rb
+++ spec/controllers/githubs_controller_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
-Rspec.describe GithubsController, type: :controller do
+RSpec.describe GithubsController, type: :controller do
+    it "#index" do
+        # get :index
+        expect(response.status).to be 200
+    end
 end
