26aca5cac40a52b5f57f9f596f7ee9fc87166802
Kevin <k3vinwei@gmail.com>
2020-10-14 03:39:58 +0800

    ddl bind github repository

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,7 +3,16 @@ require "github.rb"
 class RepositoriesController < ApplicationController
 
     def index
-        
+        # 判斷是否越獄 是的話導走
+        begin
+            @user = Gittoke.find( current_user.id )
+        rescue
+            redirect_to "/dashboard"
+        end
+
+        @repository = Github.new.getuserrepository(@user.token)
+        # puts @repository
+        # render json: @repository
     end
 
     def create
