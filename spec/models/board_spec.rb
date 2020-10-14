05b028c7b218a5b19316894ab2906bda0f5d5fa1
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-10-14 10:02:08 +0800

    <feat> [Rspec] group model test

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -4,10 +4,7 @@ RSpec.describe Board, type: :model do
   let(:user1) { create(:user) }
   let(:workspace1) { create(:workspace, user_id: user1.id) }  
   it "board BL to workspace" do
-    board = Board.create(name: "test board", workspace_id: workspace1.id)
-    expect(board).to eq(workspace1.boards.last)
+    board1 = Board.create(name: "test board", workspace_id: workspace1.id)
+    expect(board1).to eq(workspace1.boards.last)
   end
-
-  # it "" do
-  # end
 end
