b28498e09fc4fbda8bf66769c76360b5cf6201db
Kevin <k3vinwei@gmail.com>
2020-10-14 16:56:45 +0800

    fix find => where

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,7 +5,7 @@ class RepositoriesController < ApplicationController
     def index
         # 判斷是否越獄 是的話導走
         begin
-            @user = Gittoke.find(current_user.id )
+            @user = Gittoke.where("user_id = #{current_user.id}")
         rescue
             redirect_to "/dashboard"
         end
@@ -13,7 +13,7 @@ class RepositoriesController < ApplicationController
         @repository = Github.new.getuserrepository(@user.token)
 
         begin
-            @nowrepository = GitUser.find( current_user.id )
+            @nowrepository = GitUser.where("user_id = #{current_user.id}")
         rescue
             @nowrepository = GitUser.new
             @nowrepository.repository = "無設定"
@@ -23,7 +23,7 @@ class RepositoriesController < ApplicationController
     def create
 
         begin
-            @user = GitUser.find( params[:id] )
+            @user = GitUser.where("user_id = #{params[:id]}")
 
             @user.repository = params[:repository]
             @user.org = params[:org]
