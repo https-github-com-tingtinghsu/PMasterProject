3812af7859c1662f10645101370b90bb67252f3e
Kevin <k3vinwei@gmail.com>
2020-10-15 16:09:42 +0800

    params .present?

--- app/controllers/githubs_controller.rb
+++ app/controllers/githubs_controller.rb
@@ -2,36 +2,38 @@ require "github.rb"
 
 class GithubsController < ApplicationController
     def index
-        access_token = Github.new.gettoken(params["code"])
+        if params["code"].present?
+            access_token = Github.new.gettoken(params["code"])
 
-        # DB
-        puts "這是寫入 User:"
-        puts current_user.id
+            # DB
+            # puts "這是寫入 User:"
+            # puts current_user.id
 
-        @user = Gittoke.find_by("user_id = ?", current_user.id)
-        if @user != nil
-            if @user.token != access_token
+            @user = Gittoke.find_by("user_id = ?", current_user.id)
+            if @user != nil
+                if @user.token != access_token
+                    @user.token = access_token
+
+                    @user.save
+                end
+            else
+                @user = Gittoke.new
                 @user.token = access_token
+                @user.user_id = current_user.id
 
                 @user.save
             end
-            puts "find user"
+        
+            # for test
+            puts "這是寫入 Session:"
+            session[:user] = access_token
+            puts "這是session[:user] 當前的值："
+            puts session[:user]
+
+            @checkOA = true
+            redirect_to "/repositories"
         else
-            @user = Gittoke.new
-            @user.token = access_token
-            @user.user_id = current_user.id
-
-            @user.save
-            puts "not find"
+            redirect_to "/"
         end
-      
-        # for test
-		puts "這是寫入 Session:"
-        session[:user] = access_token
-        puts "這是session[:user] 當前的值："
-        puts session[:user]
-
-        @checkOA = true
-        redirect_to "/repositories"
     end
 end
